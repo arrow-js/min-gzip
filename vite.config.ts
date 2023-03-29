f751c23db10a4af8f38bc0409210418050edff96
Andrew Boyd <andrew@formkit.com>
2023-03-29 17:03:17 -0400

    adds top-level-await plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 export default defineConfig({
   optimizeDeps: {
-    exclude: ["brotli-wasm", "brotli-wasm/pkg.bundler/brotli_wasm_bg.wasm"],
+    exclude: ['brotli-wasm', 'brotli-wasm/pkg.bundler/brotli_wasm_bg.wasm'],
   },
+  plugins: [topLevelAwait()],
 })
